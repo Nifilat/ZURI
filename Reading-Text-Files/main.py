d680af1a85282f589171b58419556910fc5d20d0
OLUWANIFEMI <latunde.nifemi2016@gmail.com>
2022-05-28 23:22:52 +0100

    Update main.py

--- Reading-Text-Files/main.py
+++ Reading-Text-Files/main.py
@@ -3,15 +3,40 @@
 # count_words("The cake is done. It is a big cake!")
 # --> {"cake":2, "big":1, "is":2, "the":1, "a":1, "it":1}
 
+import re
+
+
 def read_file_content(filename):
+    f = open(filename, "r")
+    read = f.read()
+    f.close()
 
-    return 'Hello World'
+    return read
 
-    # [assignment] Add your code here
+
+print(read_file_content("story.txt"))
+
+
+# [assignment] Add your code here
 
 
 def count_words():
-    text = read_file_content("./story.txt")
-    # [assignment] Add your code here
+    txt = read_file_content("./story.txt")
+    lowercase = txt.lower()
+    text_count = {}
 
-    return {"as": 10, "would": 20}
+    split_txt = re.split('[?.,\n\t&! ]', lowercase)
+
+    for word in split_txt:
+        if word in text_count:
+            text_count[word] += 1
+        else:
+            text_count[word] = 1
+
+    return text_count
+
+
+print(count_words())
+
+
+# [assignment] Add your code here
