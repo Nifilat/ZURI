609232729a134eba3abf79ea39479403cc12cd4f
OLUWANIFEMI <latunde.nifemi2016@gmail.com>
2022-06-08 18:34:53 +0100

    Create main.py

---
+++ Rock-Paper-Scissors/main.py
@@ -0,0 +1,52 @@
+import random
+
+
+while True:
+    print("Welcome to a simple Rock-Paper-Scissors game\n")
+    R = 'rock'
+    P = 'paper'
+    S = 'scissors'
+    possible_choices = [R, P, S]
+
+    print("R for Rock,\nP for paper, and\nS for scissors \n")
+    user_choice = input("Enter a choice: (R, P, S): ")
+    if user_choice == 'R':
+        user_choice == R
+    elif user_choice == 'P':
+        user_choice == P
+    elif user_choice == 'S':
+        user_choice == S
+
+    if user_choice not in ('R', 'S', 'P'):
+        print("Invalid Input, choose from the choices below:\n")
+        continue
+
+    else:
+        computer_choice = random.choice(possible_choices)
+        print("Computer chose " + computer_choice)
+        print(f"Player ({user_choice}) : CPU ({computer_choice}).")
+
+        if user_choice == computer_choice:
+            print(f"Both players selected {user_choice}. It's a tie!")
+
+        elif user_choice == 'R':
+            if computer_choice == "scissors":
+                print("Rock smashes scissors! You win!")
+            else:
+                print("Paper covers rock! You lose.")
+
+        elif user_choice == 'P':
+            if computer_choice == "rock":
+                print("Paper covers rock! You win!")
+            else:
+                print("Scissors cuts paper! You lose.")
+
+        elif user_choice == 'S':
+            if computer_choice == 'paper':
+                print("Scissors cuts paper! You win!")
+            else:
+                print("Rock smashes scissors! You lose.")
+
+        play_again = input("Play again? (y/n): ")
+        if play_again.lower() != "y":
+            break
